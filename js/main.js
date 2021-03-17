315609047e3e82360dce55d44c7f4348593fd2a2
icanjob <71077573+icangoodjob@users.noreply.github.com>
2021-03-17 22:29:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(function(){
+	$('.slider').slick({
+		arrows: false,
+		fade: true,
+		autoplay: 3000,
+		dots: true
+	});
+
+	$('.header-btn').on('click', function(){
+	$('.menu').addClass('active');
+});
+	$('.close-btn').on('click', function(){
+		$('.menu').removeClass('active');
+	});
+});
+
+var videoTrigger = document.getElementsByClassName("video__btn")[0];
+var videoPlay = document.getElementsByClassName("video__content")[0];
+
+function videoStart() {
+	videoPlay.style.display="block";
+	videoTrigger.style.display="none";
+}
+'use strict';
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+r(function(){
+    if (!document.getElementsByClassName) {
+        // Поддержка IE8
+        var getElementsByClassName = function(node, classname) {
+            var a = [];
+            var re = new RegExp('(^| )'+classname+'( |$)');
+            var els = node.getElementsByTagName("*");
+            for(var i=0,j=els.length; i < j; i++)
+                if(re.test(els[i].className))a.push(els[i]);
+            return a;
+        }
+        var videos = getElementsByClassName(document.body,"youtube");
+    } else {
+        var videos = document.getElementsByClassName("youtube");
+    }
+    var nb_videos = videos.length;
+    for (var i=0; i < nb_videos; i++) {
+        // Находим постер для видео, зная ID нашего видео
+        videos[i].style.backgroundImage = 'url(http://i.ytimg.com/vi_webp/' + videos[i].id + '/maxresdefault.webp)';
+        // Размещаем над постером кнопку Play, чтобы создать эффект плеера
+        var play = document.createElement("div");
+        play.setAttribute("class","play");
+        videos[i].appendChild(play);
+        videos[i].onclick = function() {
+            // Создаем iFrame и сразу начинаем проигрывать видео, т.е. атрибут autoplay у видео в значении 1
+            var iframe = document.createElement("iframe");
+            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+            if (this.getAttribute("data-params")) iframe_url+='&'+this.getAttribute("data-params");
+            iframe.setAttribute("src",iframe_url);
+            iframe.setAttribute("frameborder",'0');
+            // Высота и ширина iFrame будет как у элемента-родителя
+            iframe.style.width  = this.style.width;
+            iframe.style.height = this.style.height;
+            // Заменяем начальное изображение (постер) на iFrame
+            this.parentNode.replaceChild(iframe, this);
+        }
+    }
+});
